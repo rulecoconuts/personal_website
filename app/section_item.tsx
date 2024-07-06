37a0c673fd81668092de5dcde854bd8bf09d505c
Fejiro-A <rulecoconuts@gmail.com>
2024-07-06 01:36:04 -0400

    Made section item circles even

--- app/section_item.tsx
+++ app/section_item.tsx
@@ -6,9 +6,9 @@ interface SectionItemProps {
 export default function SectionItem({ logo, title, onClick }: SectionItemProps) {
     let width: number = 50;
     return (
-        <div className="flex flex-col items-center">
-            <div className="rounded-[100%] bg-white p-5">
-                <img src={logo} width={width} height={width} />
+        <div className="flex flex-col items-center shrink-0">
+            <div className={`relative shrink-0 rounded-[100%] bg-white w-[90px] h-[90px]`}>
+                <img src={logo} width={width} className="absolute top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%]" />
             </div>
             <div className="m-4 font-semibold font-[30px]">{title}</div>
         </div>
